1e01f6cbc4fa1f20502a687da5a144e2a067d784
Colei <ioan.coleman@icloud.com>
2014-12-01 23:15:56 +0000

    StripePainter 1

---
+++ TCProj1/TCProj1/StripePainter.h
@@ -0,0 +1,16 @@
+//StripePainter.h
+#include<string>
+using namespace std;
+
+#ifndef __STRIPEPAINTER_H_INCLUDED__   // if x.h hasn't been included yet...
+#define __STRIPEPAINTER_H_INCLUDED__   //   #define this so the compiler knows it has been included
+
+class StripePainter {
+
+
+public:
+	int minStrokes(string painting);
+
+};
+
+#endif 
